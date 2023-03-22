72f6d1675f8a0dfc80b02b11ad5e40b7aa1a6ccc
PenuelSam <penuelsamuel54@gmail.com>
2023-03-22 10:58:44 +0100

    Completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+import Topbar from './scenes/global/Topbar';
+import Sidebar from './scenes/global/Sidebar';
+import Dashboard from "./scenes/dashboard/Dashboard";
+import Team from "./scenes/team/Team";
+import Invoices from "./scenes/invoices/Invoices";
+import Contacts from "./scenes/contacts/Contacts";
+import Form from "./scenes/form/Form";
+import FAQ from "./scenes/faq/FAQ";
+import Geography from "./scenes/geography/Geography";
+import Calendar from "./scenes/calendar/Calendar";
+import Pie from "../src/scenes/pie/Pie";
+import Line from "../src/scenes/line/Line";
+import Bar from "./scenes/bar/Bar";
+
 
 function App() {
-  return (
-    <div className="app">
+  const [theme, colorMode] = useMode();
 
-    </div>
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      <div className="app">
+        <Sidebar />
+        <main className="content">
+          <Topbar />
+          <Routes>
+          <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/geography" element={<Geography />} />
+          </Routes>
+        </main>
+      </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
